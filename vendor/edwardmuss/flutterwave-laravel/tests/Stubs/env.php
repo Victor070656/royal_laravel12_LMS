89b1b51a136bb65063cd9ddeccdde6f62b76ccf2
V-PRO <ikechukwuv052@gmail.com>
2025-06-26 13:42:46 +0100

    updated test env key

--- vendor/edwardmuss/flutterwave-laravel/tests/Stubs/env.php
+++ vendor/edwardmuss/flutterwave-laravel/tests/Stubs/env.php
@@ -7,8 +7,8 @@
  */
 return [
     "prefix" => "rave",
-    "secretKey" => "FLWSECK-4127f15e63c9098402dcc7891798fb0f-X",
-    "publicKey" => "FLWPUBK-1cf610974690c2560cb4c36f4921244a-X",
+    "secretKey" => "FLWSECK_TEST-f937a637a169210afca61ddbfaba0ad6-X",
+    "publicKey" => "FLWPUBK_TEST-f4b43a2d9868b3754826fb550a2710e6-X",
     "title" => "Rave Payment Gateway",
     "env" => "testing",
     "logo" => "https://files.readme.io/ee907a0-small-rave_by_flutterwave.png"
